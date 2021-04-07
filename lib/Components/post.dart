a83230c523d28cad46aa9d9dd1d97378e6c7b1a7
Aaron-cly <cheuklong.y@gmail.com>
2021-04-07 23:16:44 +0800

    Added loading animation for posts

--- lib/Components/post.dart
+++ lib/Components/post.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import "package:like_button/like_button.dart";
 // import 'package:path_provider/path_provider.dart';
 import 'package:network_to_file_image/network_to_file_image.dart';
+import 'package:loading_animations/loading_animations.dart';
 import "../Constants/constants.dart";
 
 class Post extends StatefulWidget {
@@ -44,7 +45,7 @@ class _Post extends State<Post> {
   Widget build(BuildContext context) {
     var screenHeight = MediaQuery.of(context).size.height;
     var screenWidth = MediaQuery.of(context).size.width;
-    print('built');
+    print('post built');
     return Container(
       height: screenHeight * postArea,
       width: screenWidth,
@@ -52,59 +53,73 @@ class _Post extends State<Post> {
       child: Column(
         children: [
           // Image
-          Container(
-            height: screenHeight * imageArea,
-            // image background
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                // image: AssetImage("assets/images/logCard2.jpeg"),
-                image: NetworkToFileImage(url: widget.imageURL),
-                // fit makes the image occupies the whole allocated space
-                fit: BoxFit.cover,
-              ),
-            ),
-            // username
-            child: Align(
-              alignment: Alignment.bottomLeft,
-              child: Padding(
-                  padding: const EdgeInsets.only(
-                    left: 10,
-                    bottom: 10,
-                  ),
-                  child: Container(
-                    height: screenHeight * usernameArea,
-                    width: screenWidth * 0.4,
-                    child: Stack(
-                      children: [
-                        Container(
-                          height: screenHeight * usernameArea,
-                          width: screenHeight * usernameArea,
-                          // color: Colors.white,
-                          decoration: BoxDecoration(
-                            image: DecorationImage(
-                              image: AssetImage("assets/images/profilePic.png"),
-                              // fit makes the image occupies the whole allocated space
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                        ),
-                        Align(
-                          alignment: Alignment.center,
-                          child: Text(
-                            widget.username,
-                            style: TextStyle(fontSize: 20, color: Colors.white),
-                          ),
-                        ),
-                      ],
+          (widget.imageURL == "")
+              ? Container(
+                  height: screenHeight * imageArea,
+                  child: Center(
+                    child: LoadingRotating.square(
+                      duration: Duration(milliseconds: 500),
                     ),
-                  )
-                  // child: Text(
-                  //   "USERNAME",
-                  //   style: TextStyle(fontSize: 20, color: Colors.white),
-                  // ),
                   ),
-            ),
-          ),
+                )
+              : Container(
+                  height: screenHeight * imageArea,
+                  // image background
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      // image: AssetImage("assets/images/logCard2.jpeg"),
+                      image: (widget.imageURL == "")
+                          ? AssetImage("assets/images/logCard.jpeg")
+                          : NetworkToFileImage(url: widget.imageURL),
+                      // fit makes the image occupies the whole allocated space
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+
+                  // username
+                  child: Align(
+                    alignment: Alignment.bottomLeft,
+                    child: Padding(
+                        padding: const EdgeInsets.only(
+                          left: 10,
+                          bottom: 10,
+                        ),
+                        child: Container(
+                          height: screenHeight * usernameArea,
+                          width: screenWidth * 0.4,
+                          child: Stack(
+                            children: [
+                              Container(
+                                height: screenHeight * usernameArea,
+                                width: screenHeight * usernameArea,
+                                // color: Colors.white,
+                                decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                    image: AssetImage(
+                                        "assets/images/profilePic.png"),
+                                    // fit makes the image occupies the whole allocated space
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                              Align(
+                                alignment: Alignment.center,
+                                child: Text(
+                                  widget.username,
+                                  style: TextStyle(
+                                      fontSize: 20, color: Colors.white),
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                        // child: Text(
+                        //   "USERNAME",
+                        //   style: TextStyle(fontSize: 20, color: Colors.white),
+                        // ),
+                        ),
+                  ),
+                ),
 
           // Location, Name and Like
           Container(
