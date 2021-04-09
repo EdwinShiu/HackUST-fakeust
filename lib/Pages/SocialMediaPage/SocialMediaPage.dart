ef5d91b3093171d0bcdaba5110310d0b38ecac6c
Aaron-cly <cheuklong.y@gmail.com>
2021-04-09 22:46:50 +0800

    Upload new post

--- lib/Pages/SocialMediaPage/SocialMediaPage.dart
+++ lib/Pages/SocialMediaPage/SocialMediaPage.dart
@@ -4,9 +4,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 import '../../Constants/constants.dart';
 import '../../states/currentUser.dart';
-import '../../Cards/travellogCard.dart';
 import '../../Components/post.dart';
 import './UploadPost.dart';
+import '../../models/new_post.dart';
 
 class SocialMediaPage extends StatefulWidget {
   // const SocialPage({Key? key}) : super(key: key);
@@ -24,6 +24,8 @@ class _SocialMediaPage extends State<SocialMediaPage> {
 
   void addPost(String uid) {
     print("UPLOAD POST");
+    NewPost _newPost = Provider.of<NewPost>(context, listen: false);
+    _newPost.initTags();
     Navigator.push(
       context,
       MaterialPageRoute(
@@ -38,63 +40,66 @@ class _SocialMediaPage extends State<SocialMediaPage> {
     // var screenWidth = MediaQuery.of(context).size.width;
     String uid = Provider.of<CurrentUser>(context).getUid;
 
-    return Stack(children: [
-      StreamBuilder(
-        stream: FirebaseFirestore.instance.collection('posts').snapshots(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData)
-            return Text("NO POST");
-          else
-            return CustomScrollView(slivers: <Widget>[
-              SliverList(
-                delegate: SliverChildBuilderDelegate(
-                  (BuildContext context, int index) {
-                    final post =
-                        snapshot.data.docs[index % snapshot.data.docs.length];
-                    //get username by uid
-                    return FutureBuilder<DocumentSnapshot>(
-                      future: FirebaseFirestore.instance
-                          .collection('users')
-                          .doc(post['uid'])
-                          .get(),
-                      builder: (BuildContext context,
-                          AsyncSnapshot<DocumentSnapshot> snapshot) {
-                        if (snapshot.hasError) {
-                          return Post();
-                        }
+    return Stack(
+      children: [
+        StreamBuilder(
+          stream: FirebaseFirestore.instance.collection('posts').snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData)
+              return Text("NO POST");
+            else
+              return CustomScrollView(slivers: <Widget>[
+                SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                    (BuildContext context, int index) {
+                      final post =
+                          snapshot.data.docs[index % snapshot.data.docs.length];
+                      //get username by uid
+                      return FutureBuilder<DocumentSnapshot>(
+                        future: FirebaseFirestore.instance
+                            .collection('users')
+                            .doc(post['uid'])
+                            .get(),
+                        builder: (BuildContext context,
+                            AsyncSnapshot<DocumentSnapshot> snapshot) {
+                          if (snapshot.hasError) {
+                            return Post();
+                          }
 
-                        if (snapshot.connectionState == ConnectionState.done) {
-                          Map<String, dynamic> data = snapshot.data.data();
-                          if (data != null)
-                            return Post(
-                              username: data['username'],
-                              imageURL: post['image_URL'],
-                              likeCount: post['like_count'],
-                              caption: post['description'],
-                            );
-                        }
-                        return Post();
-                      },
-                    );
-                  },
+                          if (snapshot.connectionState ==
+                              ConnectionState.done) {
+                            Map<String, dynamic> data = snapshot.data.data();
+                            if (data != null)
+                              return Post(
+                                username: data['username'],
+                                imageURL: post['image_URL'],
+                                likeCount: post['like_count'],
+                                caption: post['description'],
+                              );
+                          }
+                          return Post();
+                        },
+                      );
+                    },
+                  ),
                 ),
-              ),
-            ]);
-        },
-      ),
-      // add post
-      Align(
-        alignment: Alignment.bottomRight,
-        child: Padding(
-          padding: const EdgeInsets.only(right: 15.0, bottom: 15.0),
-          child: FloatingActionButton(
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(20.0))),
-            onPressed: () => addPost(uid),
-            child: Icon(Icons.add),
+              ]);
+          },
+        ),
+        // add post
+        Align(
+          alignment: Alignment.bottomRight,
+          child: Padding(
+            padding: const EdgeInsets.only(right: 15.0, bottom: 15.0),
+            child: FloatingActionButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0))),
+              onPressed: () => addPost(uid),
+              child: Icon(Icons.add),
+            ),
           ),
         ),
-      ),
-    ]);
+      ],
+    );
   }
 }
