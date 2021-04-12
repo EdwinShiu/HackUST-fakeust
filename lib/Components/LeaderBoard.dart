b96f2d2bc5f397d98d4c6e90384ffe637b75f500
Edwin Shiu <edwin2000412@gmail.com>
2021-04-13 02:28:19 +0800

    leaderboard

--- lib/Components/LeaderBoard.dart
+++ lib/Components/LeaderBoard.dart
@@ -1,1 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
+class LeaderBoard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Get the height of the screen
+    final screenHeight = MediaQuery.of(context).size.height;
+    // Get the width of the screen
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return Container(
+      height: screenHeight * 0.15,
+      margin: EdgeInsets.symmetric(vertical: 10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15.0),
+        color: Color(0xFF44CF73),
+      ),
+      child: Align(
+        child: FutureBuilder<List<UserScore>>(
+          future: _UserDatabase().userScoreList,
+          builder: (context, snapshot) {
+            // Get user score list
+            List<UserScore> userScoreList = snapshot.data;
+            // Sort user score list by score
+            userScoreList.sort((a, b) => a.score.compareTo(b.score));
+            return Padding(
+              padding: EdgeInsets.all(5),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Flexible(
+                    child: Row(
+                      children: [
+                        Flexible(
+                          child: Text(userScoreList.length > 0
+                              ? userScoreList[0]?.username
+                              : '---'),
+                        ),
+                        Flexible(
+                          child: Text(userScoreList.length > 0
+                              ? userScoreList[0]?.score.toString()
+                              : '---'),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Flexible(
+                    child: Row(
+                      children: [
+                        Flexible(
+                          child: Text(userScoreList[1]?.username ?? '---'),
+                        ),
+                        Flexible(
+                          child:
+                              Text(userScoreList[1]?.score.toString() ?? '---'),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Flexible(
+                    child: Row(
+                      children: [
+                        Flexible(
+                          child: Text(userScoreList[2]?.username ?? '---'),
+                        ),
+                        Flexible(
+                          child:
+                              Text(userScoreList[2]?.score.toString() ?? '---'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+// Class to fetch user score
+class _UserDatabase {
+  // Collection Reference for fetching user data
+  final CollectionReference userCollection =
+      FirebaseFirestore.instance.collection('users');
+
+  // Map List<Snapshot> to List<UserScore>
+  Future<List<UserScore>> get userScoreList async {
+    return List<UserScore>.from((await _userCollectionData()).docs.map((doc) {
+      return UserScore(
+        username: doc.data()['username'] ?? '---', // null-aware
+        score: doc.data()['score'] ?? -1, // null-aware
+      );
+    }));
+  }
+
+  // private method that fetch users collection in firestore
+  Future<QuerySnapshot> _userCollectionData() async {
+    return await userCollection.get();
+  }
+}
+
+// Class for List structure
+class UserScore {
+  String username;
+  int score;
+
+  UserScore({@required this.username, @required this.score});
+}
--- lib/Pages/LandingPage/landingPage.dart
+++ lib/Pages/LandingPage/landingPage.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hackust_fakeust/Pages/SocialMediaPage/UploadPost.dart';
+import 'package:hackust_fakeust/states/currentUser.dart';
+import 'package:provider/provider.dart';
 import 'package:hackust_fakeust/Components/mapWidget.dart';
 import 'package:hackust_fakeust/Pages/SocialMediaPage/SocialMediaPage.dart';
 import 'package:hackust_fakeust/Pages/TravelLogPage/travellogpage.dart';
 import 'package:hackust_fakeust/Pages/mapPage/mapPage.dart';
+import 'package:hackust_fakeust/models/new_post.dart';
 
 import '../../Cards/travellogCard.dart';
 import '../LeaderboardPage/LeaderboardPage.dart';
@@ -16,10 +20,23 @@ class _LandingPage extends State<LandingPage> {
   int _currentIndex = 1;
   final List<Widget> stay_alive_screens = [
     MapPage(),
-    SocialMediaPage(),
+    //SocialMediaPage(),
+    Container(child: Text("s")),
     LeaderBoardPage(),
     TravelLogPage()
   ];
+
+  void addPost(String uid) {
+    print("UPLOAD POST");
+    NewPost _newPost = Provider.of<NewPost>(context, listen: false);
+    _newPost.initTags();
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => UploadPost(),
+      ),
+    );
+  }
 
   void onTabTapped(int index) {
     print("GO TO page $index");
@@ -30,6 +47,8 @@ class _LandingPage extends State<LandingPage> {
 
   @override
   Widget build(BuildContext context) {
+    String uid = Provider.of<CurrentUser>(context).getUid;
+
     return Container(
       color: Colors.white,
       child: SafeArea(
@@ -38,6 +57,14 @@ class _LandingPage extends State<LandingPage> {
             index: _currentIndex,
             children: stay_alive_screens,
           ),
+          floatingActionButton: FloatingActionButton(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(20.0))),
+            onPressed: () => addPost(uid),
+            child: Icon(Icons.add),
+          ),
+          floatingActionButtonLocation:
+              FloatingActionButtonLocation.centerDocked,
           bottomNavigationBar: BottomNavigationBar(
             elevation: 0,
             fixedColor: Colors.black,
