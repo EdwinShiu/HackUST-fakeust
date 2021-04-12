91f188cbb7b0a28b7e1877bb26c9a837f3ac99bd
KennyTsang105 <kennytps2001@gmail.com>
2021-04-13 00:22:50 +0800

    SignUpPage

---
+++ lib/Pages/SignUpPage/signupPage.dart
@@ -0,0 +1,227 @@
+import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
+
+import '../../Constants/constants.dart';
+import '../../states/currentUser.dart';
+
+class SignupPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var screenHeight = MediaQuery.of(context).size.height;
+    var screenWidth = MediaQuery.of(context).size.width;
+    print('Sign Up Page');
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        color: const Color(backgroundPrimaryColor),
+        width: screenWidth,
+        child: Column(
+          children: [
+            //logo
+            Padding(
+              padding: EdgeInsets.only(top: screenHeight * 0.15),
+              child: Center(
+                child: Container(
+                  width: 200,
+                  height: 150,
+                  child: Image.asset('assets/images/logo.png'),
+                ),
+              ),
+            ),
+            SignUpForm(),
+            SizedBox(
+              height: 50,
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: TextButton(
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(const Color(colorWhite)),
+                  minimumSize: MaterialStateProperty.all<Size>(
+                      Size(screenWidth * 0.15, 30)),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+                onPressed: () {
+                  print("Return to Sign In Page");
+                  Navigator.pop(context);
+                },
+                child: Text(
+                  "Return to Sign In Page",
+                  style: TextStyle(
+                    color: const Color(textPrimaryColor),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SignUpForm extends StatefulWidget {
+  @override
+  // _SignInForm createState() => _SignInForm();
+  State<StatefulWidget> createState() {
+    return _SignUpForm();
+  }
+}
+
+class _SignUpForm extends State<SignUpForm> {
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _email2Controller = TextEditingController();
+  TextEditingController _pwController = TextEditingController();
+  TextEditingController _pw2Controller = TextEditingController();
+  final CurrentUser _auth = CurrentUser();
+  var _formKey = GlobalKey<FormState>();
+  String error = '';
+
+  void _signupUser(
+      TextEditingController _email,
+      TextEditingController _cemail,
+      TextEditingController _pw,
+      TextEditingController _cpw,
+      BuildContext context) async {
+    try {
+      if (_formKey.currentState.validate()) {
+        print("Created account and Logged In!");
+        dynamic result =
+            await _auth.RegisterWithEmailAndPW(_email.text, _pw.text);
+        if (result == null) {
+          print('Cannot Sign up, please try again');
+        }
+        Navigator.popAndPushNamed(context, '/landing');
+      } else {
+        print("Incorrect or different email or password");
+      }
+    } catch (e) {}
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var screenHeight = MediaQuery.of(context).size.height;
+    var screenWidth = MediaQuery.of(context).size.width;
+    return Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            // email
+            Padding(
+              padding: EdgeInsets.only(
+                  top: screenHeight * 0.05, left: 15, right: 15),
+              child: TextFormField(
+                autocorrect: false,
+                controller: _emailController,
+                validator: (String value) =>
+                    value.isEmpty ? 'Enter an email' : null,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    fillColor: Colors.white,
+                    filled: true,
+                    labelText: 'Email',
+                    hintText: 'Enter valid email id as abc@gmail.com'),
+              ),
+            ),
+            // Confirm Email
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 15.0, right: 15.0, top: 15, bottom: 0),
+              //padding: EdgeInsets.symmetric(horizontal: 15),
+              child: TextFormField(
+                autocorrect: false,
+                controller: _email2Controller,
+                validator: (String value) =>
+                    (value.compareTo(_emailController.text) == 0)
+                        ? null
+                        : 'Enter the same email',
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    fillColor: Colors.white,
+                    filled: true,
+                    labelText: 'Confirm Email',
+                    hintText: 'Enter the same email again to confirm'),
+              ),
+            ),
+            // password
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 15.0, right: 15.0, top: 15, bottom: 0),
+              //padding: EdgeInsets.symmetric(horizontal: 15),
+              child: TextFormField(
+                autocorrect: false,
+                controller: _pwController,
+                validator: (String value) =>
+                    value.isEmpty ? 'Enter a password' : null,
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    fillColor: Colors.white,
+                    filled: true,
+                    labelText: 'Password',
+                    hintText: 'Enter password'),
+              ),
+            ),
+            // Confirm password
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 15.0, right: 15.0, top: 15, bottom: 0),
+              //padding: EdgeInsets.symmetric(horizontal: 15),
+              child: TextFormField(
+                autocorrect: false,
+                controller: _pw2Controller,
+                validator: (String value) =>
+                    (value.compareTo(_pwController.text) == 0)
+                        ? null
+                        : 'Enter the same password to confirm',
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    fillColor: Colors.white,
+                    filled: true,
+                    labelText: 'Confirm Password',
+                    hintText: 'Enter password again'),
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Container(
+              height: 50,
+              width: 250,
+              decoration: BoxDecoration(
+                  color: Colors.greenAccent,
+                  borderRadius: BorderRadius.circular(20)),
+              child: TextButton(
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(const Color(colorWhite)),
+                  minimumSize: MaterialStateProperty.all<Size>(
+                      Size(screenWidth * 0.65, 50)),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+                onPressed: () {
+                  _signupUser(_emailController, _email2Controller,
+                      _pwController, _pw2Controller, context);
+                  // print('Register Account');
+                },
+                child: Text(
+                  'Register Account',
+                  style: TextStyle(
+                      color: const Color(textPrimaryColor), fontSize: 25),
+                ),
+              ),
+            ),
+          ],
+        ));
+  }
+}
