0eec91bb0ab0283231a674e7d82fead316bc1f14
Edwin Shiu <edwin2000412@gmail.com>
2021-04-14 01:13:30 +0800

    LeaderBoard Done

--- lib/Components/LeaderBoard.dart
+++ lib/Components/LeaderBoard.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:loading_animations/loading_animations.dart';
+import 'dart:math';
 
 class LeaderBoard extends StatelessWidget {
   @override
@@ -11,6 +13,91 @@ class LeaderBoard extends StatelessWidget {
     // Get the width of the screen
     final screenWidth = MediaQuery.of(context).size.width;
 
+    Future<void> _handleClickMe(List<UserScore> userScoreList) async {
+      return showDialog<void>(
+        context: context,
+        barrierDismissible: true,
+        builder: (BuildContext context) {
+          return Dialog(
+              backgroundColor: Colors.transparent,
+              insetPadding: EdgeInsets.all(10),
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  Container(
+                    width: screenWidth * 0.9,
+                    height: screenHeight * 0.8,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Color(0xFFFFFFFF)),
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        Text(
+                          'LeaderBoard',
+                          style: Theme.of(context).textTheme.headline2,
+                        ),
+                        Divider(
+                          indent: 20,
+                          endIndent: 20,
+                          height: 3,
+                          color: Color(0xAA666666),
+                        ),
+                        Expanded(
+                          child: ListView.builder(
+                            itemCount:
+                                min(userScoreList.length, 100), // Show top 100
+                            itemBuilder: (context, index) {
+                              return Row(
+                                mainAxisSize: MainAxisSize.max,
+                                children: [
+                                  SizedBox(
+                                    width: 40,
+                                    height: 40,
+                                    child: Center(
+                                      child: Text(
+                                        (index + 1).toString(),
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .bodyText1,
+                                      ),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: Text(
+                                      userScoreList[index]
+                                          ?.username
+                                          ?.toString(),
+                                      style:
+                                          Theme.of(context).textTheme.bodyText1,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 80,
+                                    height: 40,
+                                    child: Center(
+                                      child: Text(
+                                        userScoreList[index]?.score?.toString(),
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .bodyText1,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              );
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ));
+        },
+      );
+    }
+
     return Container(
       height: screenHeight * 0.15,
       margin: EdgeInsets.symmetric(vertical: 10),
@@ -18,20 +105,23 @@ class LeaderBoard extends StatelessWidget {
         borderRadius: BorderRadius.circular(15.0),
         color: Color(0xFF44CF73),
       ),
-      child: Align(
-        child: FutureBuilder<List<UserScore>>(
-          future: _UserDatabase().userScoreList,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState != ConnectionState.done) {
-              return LoadingRotating.square(
-                duration: Duration(milliseconds: 500),
-              );
-            } else {
-              // Get user score list
-              List<UserScore> userScoreList = snapshot.data;
-              // Sort user score list by score
-              userScoreList.sort((a, b) => b.score.compareTo(a.score));
-              return Padding(
+      child: FutureBuilder<List<UserScore>>(
+        future: _UserDatabase().userScoreList,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState != ConnectionState.done) {
+            return LoadingRotating.square(
+              duration: Duration(milliseconds: 500),
+            );
+          } else {
+            // Get user score list
+            List<UserScore> userScoreList = snapshot.data;
+            // Sort user score list by score
+            userScoreList.sort((a, b) => b.score.compareTo(a.score));
+            return InkWell(
+              onTap: () {
+                _handleClickMe(userScoreList);
+              },
+              child: Padding(
                 padding: EdgeInsets.symmetric(horizontal: 5, vertical: 8),
                 child: Column(
                   mainAxisSize: MainAxisSize.max,
@@ -121,10 +211,10 @@ class LeaderBoard extends StatelessWidget {
                     ),
                   ],
                 ),
-              );
-            }
-          },
-        ),
+              ),
+            );
+          }
+        },
       ),
     );
   }
