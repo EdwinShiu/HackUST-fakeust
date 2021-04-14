d93b6c2356b4614e88879e601fa313a6ec2a6675
KennyTsang105 <kennytps2001@gmail.com>
2021-04-14 23:27:18 +0800

    sign in and sign up UI

--- lib/Pages/SignUpPage/signupPage.dart
+++ lib/Pages/SignUpPage/signupPage.dart
@@ -11,7 +11,7 @@ class SignupPage extends StatelessWidget {
     var screenWidth = MediaQuery.of(context).size.width;
     print('Sign Up Page');
     return Scaffold(
-      backgroundColor: Colors.teal,
+      backgroundColor: Color.fromRGBO(239, 242, 200, 1),
       resizeToAvoidBottomInset: true,
       body: SingleChildScrollView(
         child: GestureDetector(
@@ -19,12 +19,12 @@ class SignupPage extends StatelessWidget {
             FocusScope.of(context).unfocus();
           },
           child: Container(
-            color: Colors.grey,
+            color: Color.fromRGBO(239, 242, 200, 1),
             width: screenWidth,
             child: Column(
               children: [
                 //logo
-                Padding(
+                /*  Padding(
                   padding: EdgeInsets.only(top: screenHeight * 0.15),
                   child: Center(
                     child: Container(
@@ -33,9 +33,10 @@ class SignupPage extends StatelessWidget {
                       child: Image.asset('assets/images/logo.png'),
                     ),
                   ),
-                ),
+                ), */
                 SignUpForm(),
-                SizedBox(
+
+                /* SizedBox(
                   height: 50,
                 ),
                 Align(
@@ -63,7 +64,7 @@ class SignupPage extends StatelessWidget {
                       ),
                     ),
                   ),
-                ),
+                ), */
               ],
             ),
           ),
@@ -99,27 +100,47 @@ class _SignUpForm extends State<SignUpForm> {
         barrierDismissible: false,
         builder: (context) {
           return AlertDialog(
+            shape: (RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(2),
+              //  side: BorderSide(color: Colors.black, width: 2)
+            )),
+            contentPadding: EdgeInsets.fromLTRB(50.0, 50.0, 50.0, 50.0),
+            backgroundColor: Color.fromRGBO(239, 242, 200, 1),
             title: Text("Enter your username:"),
             content: TextField(
               autocorrect: false,
               controller: username,
               decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  fillColor: Colors.white,
+                  contentPadding: new EdgeInsets.symmetric(
+                      vertical: 30.0, horizontal: 40.0),
+                  border: OutlineInputBorder(
+                    borderRadius: new BorderRadius.circular(60.0),
+                  ),
+                  fillColor: Color.fromRGBO(239, 242, 200, 1),
                   filled: true,
                   labelText: 'Username',
+                  labelStyle: TextStyle(
+                    color: Color.fromRGBO(8, 47, 69, 1),
+                    fontSize: 15,
+                  ),
                   hintText: 'Enter your username'),
             ),
             actions: <Widget>[
               MaterialButton(
-                  elevation: 5.0,
-                  child: Text('Return'),
+                  minWidth: 100,
+                  height: 50,
+                  color: Color.fromRGBO(236, 238, 217, 1),
+                  elevation: 0,
+                  child: Text('RETURN'),
                   onPressed: () {
                     Navigator.pop(context);
                   }),
               MaterialButton(
-                elevation: 5.0,
-                child: Text('Submit'),
+                minWidth: 100,
+                height: 50,
+                color: Color.fromRGBO(161, 246, 170, 1),
+                elevation: 0,
+                child: Text('SUBMIT'),
                 onPressed: () async {
                   dynamic result = await _auth.RegisterWithEmailAndPW(
                       email.text, password.text, username.text);
@@ -134,6 +155,7 @@ class _SignUpForm extends State<SignUpForm> {
                     "username": result.getUsername,
                     "uid": uid,
                   });
+                  print("Created account and Logged In!");
                   Navigator.popAndPushNamed(context, '/landing');
                 },
               ),
@@ -151,7 +173,6 @@ class _SignUpForm extends State<SignUpForm> {
       BuildContext context) async {
     try {
       if (_formKey.currentState.validate()) {
-        print("Created account and Logged In!");
         Navigator.pop(UsernamePopUp(context, _email, _username, _pw));
         //if (result == null) {
         print('Cannot Sign up, please try again');
@@ -174,17 +195,26 @@ class _SignUpForm extends State<SignUpForm> {
             // email
             Padding(
               padding: EdgeInsets.only(
-                  top: screenHeight * 0.05, left: 15, right: 15),
+                  top: screenHeight * 0.25, left: 15, right: 15),
               child: TextFormField(
                 autocorrect: false,
                 controller: _emailController,
                 validator: (String value) =>
                     value.isEmpty ? 'Enter an email' : null,
                 decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    fillColor: Colors.white,
-                    filled: true,
-                    labelText: 'Email',
+                    contentPadding: new EdgeInsets.symmetric(
+                        vertical: 30.0, horizontal: 10.0),
+                    fillColor: Color.fromRGBO(211, 241, 206, 1),
+                    filled: true, // <- this is required.
+                    border: OutlineInputBorder(
+                      borderRadius: new BorderRadius.circular(60.0),
+                      // borderSide: new BorderSide(),
+                    ),
+                    labelText: '     Email',
+                    labelStyle: TextStyle(
+                      color: Color.fromRGBO(8, 47, 69, 1),
+                      fontSize: 20,
+                    ),
                     hintText: 'Enter valid email id as abc@gmail.com'),
               ),
             ),
@@ -201,10 +231,19 @@ class _SignUpForm extends State<SignUpForm> {
                         ? null
                         : 'Enter the same email',
                 decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    fillColor: Colors.white,
-                    filled: true,
-                    labelText: 'Confirm Email',
+                    contentPadding: new EdgeInsets.symmetric(
+                        vertical: 30.0, horizontal: 10.0),
+                    fillColor: Color.fromRGBO(211, 241, 206, 1),
+                    filled: true, // <- this is required.
+                    border: OutlineInputBorder(
+                      borderRadius: new BorderRadius.circular(60.0),
+                      // borderSide: new BorderSide(),
+                    ),
+                    labelText: '   Confirm Email',
+                    labelStyle: TextStyle(
+                      color: Color.fromRGBO(8, 47, 69, 1),
+                      fontSize: 20,
+                    ),
                     hintText: 'Enter the same email again to confirm'),
               ),
             ),
@@ -220,10 +259,19 @@ class _SignUpForm extends State<SignUpForm> {
                     value.isEmpty ? 'Enter a password' : null,
                 obscureText: true,
                 decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    fillColor: Colors.white,
-                    filled: true,
-                    labelText: 'Password',
+                    contentPadding: new EdgeInsets.symmetric(
+                        vertical: 30.0, horizontal: 10.0),
+                    fillColor: Color.fromRGBO(211, 241, 206, 1),
+                    filled: true, // <- this is required.
+                    border: OutlineInputBorder(
+                      borderRadius: new BorderRadius.circular(60.0),
+                      // borderSide: new BorderSide(),
+                    ),
+                    labelText: '   Password',
+                    labelStyle: TextStyle(
+                      color: Color.fromRGBO(8, 47, 69, 1),
+                      fontSize: 20,
+                    ),
                     hintText: 'Enter password'),
               ),
             ),
@@ -241,10 +289,19 @@ class _SignUpForm extends State<SignUpForm> {
                         : 'Enter the same password to confirm',
                 obscureText: true,
                 decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    fillColor: Colors.white,
-                    filled: true,
-                    labelText: 'Confirm Password',
+                    contentPadding: new EdgeInsets.symmetric(
+                        vertical: 30.0, horizontal: 10.0),
+                    fillColor: Color.fromRGBO(211, 241, 206, 1),
+                    filled: true, // <- this is required.
+                    border: OutlineInputBorder(
+                      borderRadius: new BorderRadius.circular(60.0),
+                      // borderSide: new BorderSide(),
+                    ),
+                    labelText: '   Confirm Password',
+                    labelStyle: TextStyle(
+                      color: Color.fromRGBO(8, 47, 69, 1),
+                      fontSize: 20,
+                    ),
                     hintText: 'Enter password again'),
               ),
             ),
@@ -259,13 +316,14 @@ class _SignUpForm extends State<SignUpForm> {
                   borderRadius: BorderRadius.circular(20)),
               child: TextButton(
                 style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all<Color>(const Color(colorWhite)),
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                      Color.fromRGBO(161, 246, 170, 1)),
                   minimumSize: MaterialStateProperty.all<Size>(
                       Size(screenWidth * 0.65, 50)),
                   shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                     RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
+                      borderRadius: BorderRadius.circular(2),
+                      //  side: BorderSide(color: Colors.black, width: 2)
                     ),
                   ),
                 ),
@@ -277,10 +335,43 @@ class _SignUpForm extends State<SignUpForm> {
                       _pw2Controller,
                       _usernameController,
                       context);
-                  print('Register Account');
+                  print('submit username');
                 },
                 child: Text(
-                  'Register Account',
+                  'SIGN UP',
+                  style: TextStyle(
+                      color: const Color(textPrimaryColor), fontSize: 25),
+                ),
+              ),
+            ),
+            SizedBox(height: 60.0),
+
+            Container(
+              height: 50,
+              width: 250,
+
+              // decoration: BoxDecoration(
+              //     color: Colors.white, borderRadius: BorderRadius.circular(20)),
+              child: TextButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                      Color.fromRGBO(236, 238, 217, 1)),
+                  minimumSize: MaterialStateProperty.all<Size>(
+                      Size(screenWidth * 0.65, 50)),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(2),
+                      //  side: BorderSide(color: Colors.black, width: 2)
+                    ),
+                  ),
+                  // shape: MaterialStateProperty.all<StadiumBorder>(StadiumBorder()),
+                ),
+                onPressed: () {
+                  print('Go to sign up page');
+                  Navigator.pop(context);
+                },
+                child: Text(
+                  'GO BACK',
                   style: TextStyle(
                       color: const Color(textPrimaryColor), fontSize: 25),
                 ),
